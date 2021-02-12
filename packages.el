240cffa32d4ab1ed0a0ed835a2cc02da365b1089
Simen Holmestad <simehol@stud.ntnu.no>
2021-02-12 14:33:42 +0100

    Create literate config and change packages

--- packages.el
+++ packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+(package! evil-snipe :disable t)
+(package! anaconda :disable t)
+(package! default-text-scale)
